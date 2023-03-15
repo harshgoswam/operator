4eec9b857164ee602bec9e594097fd8ce5d340af
harshgoswam <122149827+harshgoswam@users.noreply.github.com>
2023-03-15 14:36:13 +0530

    Add files via upload

---
+++ operator1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+main()
+{
+	
+	int x,y,xy;  
+	
+	printf("enter value of X=");
+	
+	scanf("%d",&x);
+	
+	printf("enter value of Y=");
+	
+	scanf("%d",&y);
+	
+	xy=(x*x)+2*(x*y)+(y*y);
+	
+	printf("xy=%d",xy);
+	
+	
+
+	
+	
+
+
+
+}
---
+++ operator7.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+main()
+{
+	
+	int x,y,z,xyz;  
+	
+	printf("enter value of X=");
+	
+	scanf("%d",&x);
+	
+	printf("enter value of Y=");
+	
+	scanf("%d",&y);
+	
+	printf("enter value of Z=");
+	
+	scanf("%d",&z);
+	
+	xyz=(x*x)+(y*y)+(z*z)+2*(x*y)+2*(y*z)+2*(z*x);
+	
+	printf("xy=%d",xyz);
+	
+	
+
+	
+	
+
+
+
+}
