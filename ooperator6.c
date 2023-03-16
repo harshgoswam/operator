8276b35eff2d311c73bb6c1664fc6869c468a12e
harshgoswam <122149827+harshgoswam@users.noreply.github.com>
2023-03-16 17:56:31 +0530

    Add files via upload

---
+++ ooperator6.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+main()
+{
+	
+	int x,y,z,xyz;  
+	
+	printf("enter value of X=");
+	
+	scanf("%d",&x);
+	
+	printf("enter value of Y=");
+	
+	scanf("%d",&y);
+	
+	printf("enter value of Z=");
+	
+	scanf("%d",&z);
+	
+	xyz= x*x*x-y*y*y-z*z*z-3*x*x*y+3*x*y*y-3*y*y*z-3*y*z*z+3*x*z*z-3*x*x*z+6*x*y*z;
+
+	
+	printf("xy=%d",xyz);
+	
+	
+
+
+}
+
